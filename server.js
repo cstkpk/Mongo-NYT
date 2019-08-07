2d15b87ee9259ecd252e08dca494419245114ffa
Cara Sunberg <cnsunberg@gmail.com>
2019-08-07 18:47:13 -0400

    Set up server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+// Dependencies
+const express = require("express");
+const exphbs = require("express-handlebars");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+// Require all models
+const db = require("./models");
+
+const PORT = 3000;
+
+// Initialize Express
+const app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+// Handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+      defaultLayout: "main"
+    })
+);
+app.set("view engine", "handlebars");
+  
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on http://localhost:" + PORT + "!");
+});
